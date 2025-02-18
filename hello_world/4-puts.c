87ed26cbcafa44ed0986acd1346e89bb2a85eea1
force-push-main <10535@holbertonstudents.com>
2025-02-18 01:04:48 +0000

    placated betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: prints text using puts
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
