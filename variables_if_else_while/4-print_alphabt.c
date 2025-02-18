f77c1a12421037825db446529507d3afd16c7f2d
force-push-main <10535@holbertonstudents.com>
2025-02-18 04:15:17 +0000

    added code for task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: putchar a to z not q or e in for loop
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);	
+		}
+	}
+	putchar('\n');
+	return (0);
+}
