5029b79966fec7a0282d8c0f7bc241f0d77d74bd
force-push-main <10535@holbertonstudents.com>
2025-02-19 02:58:06 +0000

    added code for task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ *
+ * Description: checks if number is pos, neg, zero
+ * @n: passed jumber to be checked
+ * Return: returns 1, -1 or 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
