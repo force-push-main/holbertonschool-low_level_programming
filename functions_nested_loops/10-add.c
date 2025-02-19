97ebe18cf85756d52100b6e353372d745717b94a
force-push-main <10535@holbertonstudents.com>
2025-02-19 05:12:50 +0000

    added code for task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Description: prints alphabet and new line 10x
+ * @a: first int to add
+ * @b: second int to add
+ * Return: returns sum of ints
+ */
+
+int add(int a, int b)
+{
+	return ((a + b));
+}
