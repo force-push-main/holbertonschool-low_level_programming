0005cc80c32cc52231958b8ff326bf73adfb3366
force-push-main <10535@holbertonstudents.com>
2025-02-19 04:32:25 +0000

    added code for task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Description: prints every min of jacks day
+ * Return: returns void
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int h;
+	int hh;
+	int m;
+	int mm;
+
+	h = 48;
+	hh = 48;
+	m = 48;
+	mm = 48;
+
+	for (i = 0; i <= 1440; i++)
+	{
+		_putchar(h);
+		_putchar(hh);
+		_putchar(58);
+		_putchar(m);
+		_putchar(mm);
+
+		if (mm <= 56)
+		{
+			mm++;
+		}
+		else
+		{
+			mm = 48;
+			m++;
+
+			if (m >= 55)
+			{
+				m = 48;
+				hh++;
+				if (hh >= 58)
+				{
+					hh = 48;
+					h++;
+				}
+			}
+		}
+	}
+}
