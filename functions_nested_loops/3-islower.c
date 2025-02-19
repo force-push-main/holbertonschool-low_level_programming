8c87f1c30cab8a9a909978c619613156f54db577
force-push-main <10535@holbertonstudents.com>
2025-02-19 02:13:08 +0000

    added code for task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Description: checks if passed letter is lowercase
+ * @c: passed letter to be checked
+ * Return: always returns void
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	       return (1);
+	}
+	return (0);
+}
