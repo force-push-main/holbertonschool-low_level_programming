9630d0428c739def4ed2bde0f72bd099b1cb233d
force-push-main <10535@holbertonstudents.com>
2025-02-19 02:34:00 +0000

    added code for task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * Description: checks if passed letter is alphabet
+ * @c: passed letter to be checked
+ * Return: returns 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	return (0);
+}
