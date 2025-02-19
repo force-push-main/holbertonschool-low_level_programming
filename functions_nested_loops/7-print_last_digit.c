42ddd7974582931d5cd970ec1cddf082d23e5ba5
force-push-main <10535@holbertonstudents.com>
2025-02-19 03:26:19 +0000

    added code for task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * Description: prints last digit of int
+ * @r: int to extract last digit from
+ * Return: returns 1 or 0
+ */
+
+int print_last_digit(int r)
+{
+	int lastNum;
+
+	lastNum = r % 10;
+
+	return (lastNum);
+}
