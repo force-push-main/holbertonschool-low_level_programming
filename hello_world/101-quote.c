f3f6e62cc3e63f0b72bf289cf7c8812a98fd8360
force-push-main <10535@holbertonstudents.com>
2025-02-20 03:18:50 +0000

    held speaker outside of bettys window in the rain

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,6 +1,13 @@
 #include <unistd.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Description: writes quote to standard error
+ * Return: always returns 1
+ */
+
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
