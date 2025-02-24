218c7be8da2c0e70422d9d59e9d198974ae002db
force-push-main <10535@holbertonstudents.com>
2025-02-24 01:30:13 +0000

    added folder for more functions and loops

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: prints alphabet and new line
+ * Return: always returns void
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
