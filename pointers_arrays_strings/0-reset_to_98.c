fb0776e351fdce5a0c88a8f182e5704cc36f1a96
force-push-main <10535@holbertonstudents.com>
2025-02-24 23:30:09 +0000

    created dir for pointer project

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - Entry point
+ *
+ * Description: resets value of n to 98
+ * @n: pointer for n being passed
+ * Return: always returns void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
