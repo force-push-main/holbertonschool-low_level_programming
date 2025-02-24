2ca466e768fa0ecb67b34cd00d2e40eb3202c5da
force-push-main <10535@holbertonstudents.com>
2025-02-24 03:36:46 +0000

    added code for task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints 1-100, or fizz or buzz or fizz buzz if multiple
+ * Return: always returns void
+ */
+
+int main (void)
+{
+	int i;
+	i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+			{
+				printf("FizzBuzz");
+			}
+			else
+			{
+			printf("Fizz");
+			}
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			putchar(i + 48);
+		}
+	}
+
+	return (0);
+}
