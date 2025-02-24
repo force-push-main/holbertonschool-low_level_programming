6a861a44114b6844fdc14bcbc045c98c2b45e9b1
force-push-main <10535@holbertonstudents.com>
2025-02-24 05:44:45 +0000

    included -1 condition in loop

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -21,16 +21,21 @@ void print_number(int n)
 		i /= 10;
 	}
 
-	while (j > 1)
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			_putchar('-');
-			n *= -1;
-		}
+		_putchar('-');
+	}
+
+	while (j >= 1)
+	{
 		k = n / j;
+
+		if (k < 0)
+		{
+			k *= -1;
+		}
+
 		_putchar(48 + (k % 10));
 		j /= 10;
 	}
-	_putchar(48 + (n % 10));
 }
