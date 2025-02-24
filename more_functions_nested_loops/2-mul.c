89c63c0d167311ab972455992cc1d4c95b4718ca
force-push-main <10535@holbertonstudents.com>
2025-02-24 01:43:14 +0000

    added code for task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * Description:
+ *  @a: first int to be multiplied
+ *  @b: second int to be multiplied
+ *  Return: returns product of two ints
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
