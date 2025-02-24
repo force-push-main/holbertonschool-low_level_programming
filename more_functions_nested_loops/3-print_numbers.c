6650e599de70caa34865f713a9b3ea13cfd48df1
force-push-main <10535@holbertonstudents.com>
2025-02-24 01:48:30 +0000

    added code for task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: prints chars 0 - 9
+ * Return: returns void
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	i = '0';
+
+	while (i <= '9')
+	{
+		_putchar(i);
+		i++;
+	}
+}
