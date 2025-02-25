b4d041cccee09f854e803088164b35ee71d2f118
force-push-main <10535@holbertonstudents.com>
2025-02-25 06:31:53 +0000

    added code for task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry point
+ *
+ * Description: prints half a string
+ * @str: string to be checked
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+
+}
