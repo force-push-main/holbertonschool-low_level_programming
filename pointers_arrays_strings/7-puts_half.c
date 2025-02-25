182f3a70d504205d538a0accaa8d04e4e991cdab
force-push-main <10535@holbertonstudents.com>
2025-02-25 06:39:25 +0000

    actually added the code for task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,5 +10,32 @@
 
 void puts_half(char *str)
 {
+	int i, n;
 
+	while (str[i])
+	{
+		i++;
+	}
+
+	if (i - 1 % 2 == 0)
+	{
+		n = (i - 1) / 2;
+
+		while (str[n])
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+
+	if (i - 1 % 2 == 1)
+	{
+		n = i / 2;
+
+		while (str[n])
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
 }
