d822e8d3ae4245d6d0641b11865b066c6e2fdb52
force-push-main <10535@holbertonstudents.com>
2025-02-26 06:07:01 +0000

    added code for task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,9 @@
+/**
+ * _strncpy - Entry point
+ *
+ * Description: copies string to dest
+ * @dest: buffer to hold string
+ * @src: string to be copied
+ * @n: number of bytes to be copied
+ * Return: void
+ */
