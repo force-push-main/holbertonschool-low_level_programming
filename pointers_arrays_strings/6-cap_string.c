9529e54fbb7a5734d118f20ce9c18a1ee7715ef9
force-push-main <10535@holbertonstudents.com>
2025-02-27 00:30:15 +0000

    added condition for first letter lower

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,6 +12,11 @@ char *cap_string(char *str)
 
 	i = 0;
 
+	if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+	{
+		str[i + 1] = str[i + 1] - 32;
+	}
+
 	while (str[i])
 	{
 		if (str[i] < 'A' || str[i] > 'z')
