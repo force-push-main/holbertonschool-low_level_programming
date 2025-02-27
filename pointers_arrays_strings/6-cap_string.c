d5e428014fc0dd93f64d462a8bab28eed71f5e6d
force-push-main <10535@holbertonstudents.com>
2025-02-27 00:26:34 +0000

    added code for task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+/**
+ * cap_string- Entry point
+ *
+ * Description: changes string to title case
+ * @str: string to be edited
+ * Return: edited string
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i])
+	{
+		if (str[i] < 'A' || str[i] > 'z')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+
+		i++;
+	}
+
+	return (str);
+}
