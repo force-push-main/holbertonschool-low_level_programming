641a6a5d3e906109b72d819fad436974d950a136
force-push-main <10535@holbertonstudents.com>
2025-03-02 23:42:27 +0000

    added code for task 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+/**
+ * _memcpy - Entry point
+ *
+ * Description: copy n number of bytes from src to dest
+ * @src: origin of data to be copied
+ * @dest: location to be copied to
+ * @n: number of times/bytes to be copied
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *dest_start = dest;
+
+	while (n--)
+	{
+		*dest = *src;
+		
+		dest++;
+		src++;
+	}
+
+	dest = dest_start;
+
+	return (dest);
+}
