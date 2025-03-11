5419c1ce10750acff60fc748c9e46a7a48c1809f
force-push-main <10535@holbertonstudents.com>
2025-03-11 02:13:23 +0000

    less than 40 lines

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,16 +19,13 @@ char *str_concat(char *s1, char *s2)
 
 	if (!s1)
 		s1 = "";
-
 	if (!s2)
 		s2 = "";
-
 	while (s1[i])
 		i++;
 
 	while (s2[j])
 		j++;
-
 	ptr = malloc(sizeof(char) * (i + j + 1));
 
 	if (!ptr)
@@ -36,20 +33,17 @@ char *str_concat(char *s1, char *s2)
 
 	i = 0;
 	j = 0;
-
 	while (s1[i])
 	{
 		ptr[i] = s1[i];
 		i++;
 	}
-
 	while (s2[j])
 	{
 		ptr[i] = s2[j];
 		j++;
 		i++;
 	}
-
 	ptr[i] = '\0';
 
 	return (ptr);
