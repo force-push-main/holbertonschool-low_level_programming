cb8f299fb1f352afafa302e3854e313d744d256c
force-push-main <10535@holbertonstudents.com>
2025-03-11 00:30:32 +0000

    added dir for malloc project and code for task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Entry point
+ *
+ * Description: creates array of chars, inits with specific char
+ * @size: size of array
+ * @c: initial char
+ * Return: returns null if fail or pointer to char[]
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+		return (0);
+
+	ptr = malloc(sizeof(c) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+
+	return (ptr);
+}
