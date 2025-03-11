f31e2d0a92071a6742dced615d900aeb21134710
force-push-main <10535@holbertonstudents.com>
2025-03-11 01:34:07 +0000

    trying i <= len

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -24,12 +24,12 @@ char *_strdup(char *str)
 		len++;
 	}
 
-	ptr = malloc(sizeof(*str) * (len + 1));
+	ptr = malloc(sizeof(char) * (len + 1));
 
 	if (!ptr)
 		return (0);
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i <= len; i++)
 		ptr[i] = str[i];
 
 	return (ptr);
