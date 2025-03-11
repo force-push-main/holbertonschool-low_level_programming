bfa126d62824c9bcd8088bcba6255ce461b21573
force-push-main <10535@holbertonstudents.com>
2025-03-11 01:59:17 +0000

    added code for task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+
+/**
+ * str_concat - Entry point
+ *
+ * Description: concats two strings and returns pointer
+ * @s1: first string
+ * @s2: second string
+ * Return: ptr to new string or null if failure
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j;
+	char *ptr;
+
+	i = 0;
+	j = 0;
+
+	while (s1[i])
+		i++;
+
+	while (s2[j])
+		j++;
+
+	ptr = malloc(sizeof(char) * (i + j + 1));
+
+	if (!ptr)
+		return (0);
+
+	i = 0;
+	j = 0;
+
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+
+	while (s2[j])
+	{
+		ptr[i] = s2[j];
+		j++;
+		i++;
+	}
+
+	ptr[i] = '\0';
+
+	return (ptr);
+}
