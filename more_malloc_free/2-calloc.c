484deeaecc494648ba2c2c246199ddb3c57aa367
force-push-main <10535@holbertonstudents.com>
2025-03-12 03:50:04 +0000

    changed end condition for loop

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -25,7 +25,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	zeroArray = ptr;
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		zeroArray[i] = 0;
 
 	return (ptr);
