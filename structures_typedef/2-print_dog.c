40d309d0ebc7821b82932a1b16e5155f1889b103
force-push-main <10535@holbertonstudents.com>
2025-03-12 06:02:13 +0000

    changed for multiple null values

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,18 +11,22 @@
 
 void print_dog(struct dog *d)
 {
+
 	if (!d)
-		;
+		return;
 
-	else if (!d->name)
-		printf("Name: (nil)\nAge: %f\nOwner: %s\n", d->age, d->owner);
-
-	else if (!d->age)
-		printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
-
-	else if (!d->owner)
-		printf("Name: %s\nAge: %f\nOwner: (nil)\n", d->name, d->age);
-
+	if (!d->name)
+		printf("Name: (nil)\n");
 	else
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		printf("Name: %s\n", d->name);
+
+	if (!d->age)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (!d->owner)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
