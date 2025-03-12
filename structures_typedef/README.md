ccada32690ab7e515a95bccd0e179e7f5eb4d442
force-push-main <10535@holbertonstudents.com>
2025-03-12 05:00:03 +0000

    added header file for task 0

---
+++ structures_typedef/README.md
@@ -0,0 +1,2 @@
+# Where a list of functions and their uses could be one day
+
---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog info struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: info struct for dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
