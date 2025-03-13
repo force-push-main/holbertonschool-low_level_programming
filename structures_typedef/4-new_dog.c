0b733fa0a12846779730d6361b80f8630ed0522e
force-push-main <10535@holbertonstudents.com>
2025-03-13 01:26:58 +0000

    have to add memory to struct

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,37 +2,46 @@
 #include "dog.h"
 
 /**
- * createPtr - helper function
+ * _strLen - helper function
  *
- * Description: creates ptr to member
- * @member: data to create pointer
+ * Description: finds length of string
+ * @name: string to find length of
+ * Return: lenght of string
+ */
+
+int _strlen(char *name)
+{
+	int i;
+
+	i = 0;
+
+	while (name[i])
+		i++;
+
+	return (i);
+}
+
+/**
+ * cpyMember - Entry point
+ *
+ * Description: creates new dog
+ * @member: member to add data to
+ * @value: value to be added member
  * Return: pointer to member data
  */
 
-char *createPtr(char *member)
+char *cpyMember(char *member, char *value)
 {
-	char *ptr;
-	int i, j;
+	int i;
 
-	while (member[i])
-		i++;
+	for (i = 0; i < _strlen(value); i++)
+		member[i] = value[i];
 
-	ptr = malloc(sizeof(char) * i + 1);
+	member[i] = '\0';
 
-	if (!ptr)
-	{
-		free(ptr);
-		return (0);
-	}
-
-	for (j = 0; j < i; j++)
-		ptr[j] = member[j];
-
-	ptr[j] = '\0';
-
-
-	return (ptr);
+	return (member);
 }
+
 
 /**
  * new_dog - Entry point
@@ -46,22 +55,33 @@ char *createPtr(char *member)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *poppy;
-	char *nameCpy, *ownerCpy;
+	dog_t *dog;
 
-	poppy = malloc(sizeof(dog_t));
-	nameCpy = createPtr(name);
-	ownerCpy = createPtr(owner);
+	if (!name || !age || !owner)
+		return (0);
 
-	if (!poppy || !nameCpy || !ownerCpy)
+	dog = malloc(sizeof(dog_t));
+
+	if (!dog)
 	{
-		free(poppy);
+		free(dog);
 		return (0);
 	}
 
-	poppy->name = nameCpy;
-	poppy->age = age;
-	poppy->owner = ownerCpy;
+	dog->name = malloc(sizeof(char) * _strlen(name) + 1);
+	dog->owner = malloc(sizeof(char) * _strlen(owner) + 1);
 
-	return (poppy);
+	if (!dog->owner || !dog->name)
+	{
+		free(dog->name);
+		free(dog->owner);
+		free(dog);
+		return (0);
+	}
+
+	dog->name = cpyMember(dog->name, name);
+	dog->age = age;
+	dog->owner = cpyMember(dog->owner, owner);
+
+	return (dog);
 }
