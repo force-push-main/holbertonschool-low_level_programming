f54b43aebf0be937ed548022b86034b04e399f4f
force-push-main <10535@holbertonstudents.com>
2025-03-13 00:22:27 +0000

    created helper function to create ptrs

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,39 @@
 #include "dog.h"
 
 /**
+ * createPtr - helper function
+ *
+ * Description: creates ptr to member
+ * @member: data to create pointer
+ * Return: pointer to member data
+ */
+
+char *createPtr(char *member)
+{
+	char *ptr;
+	int i, j;
+
+	while (member[i])
+		i++;
+
+	ptr = malloc(sizeof(char) * i + 1);
+
+	if (!ptr)
+	{
+		free(ptr);
+		return (0);
+	}
+
+	for (j = 0; j < i; j++)
+		ptr[j] = member[j];
+
+	ptr[j] = '\0';
+
+
+	return (ptr);
+}
+
+/**
  * new_dog - Entry point
  *
  * Description: creates new dog
@@ -16,20 +49,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *poppy;
 	char *nameCpy, *ownerCpy;
 
-	nameCpy = malloc(sizeof(char) * 6);
-	ownerCpy = malloc(sizeof(char) * 9);
 	poppy = malloc(sizeof(dog_t));
+	nameCpy = createPtr(name);
+	ownerCpy = createPtr(owner);
 
 	if (!poppy || !nameCpy || !ownerCpy)
 	{
-		free(nameCpy);
-		free(ownerCpy);
 		free(poppy);
 		return (0);
 	}
-
-	nameCpy = name;
-	ownerCpy = owner;
 
 	poppy->name = nameCpy;
 	poppy->age = age;
