147a3b4ce6e157c16d70008fbf4cacff6a65ba0f
force-push-main <10535@holbertonstudents.com>
2025-03-17 00:18:40 +0000

    added folder for function pointer

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Entry point
+ *
+ * Description: prints name
+ * @name: name to print
+ * @f: function pointer
+ * Return: always returns void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+
+}
