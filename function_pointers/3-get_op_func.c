3afd9794edb5832c09d0cec922684d731e3de4ab
force-push-main <10535@holbertonstudents.com>
2025-03-17 03:18:09 +0000

    added code for task 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+
+
+	while (i <= 5)
+	{
+		if (s[0] == ops[i].op[0])
+			return (ops[i].f);
+		i++;
+	}
+
+
+	return (0);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	int (*fetched_func)(int, int);
+
+	if (argc < 3 || !argv[1] || !argv[2] || !argv[3] || strlen(argv[2]) > 1)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	fetched_func = get_op_func(argv[2]);
+
+	if (!fetched_func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", fetched_func(a, b));
+
+	return (0);
+}
