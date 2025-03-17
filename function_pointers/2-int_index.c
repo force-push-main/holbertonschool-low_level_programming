600e1d102df1252aeb90eb38e71f1cb3e717bfb8
force-push-main <10535@holbertonstudents.com>
2025-03-17 00:39:57 +0000

    added code for task 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * int_index - Entry point
+ *
+ * Description: returns index of first el for which cmp != 0
+ * @array: array to pass to callback
+ * @size: size of array
+ * @cmp: callback function
+ * Return: always returns void
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]) != 0)
+			return (i);
+
+
+	return (-1);
+}
