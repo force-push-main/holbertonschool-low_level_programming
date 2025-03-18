cc22cfdd4a293ebbf72bda226c2ef26d504eccc2
force-push-main <10535@holbertonstudents.com>
2025-03-18 01:57:42 +0000

    added code for task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - entry point
+ *
+ * Description: prints passed nums separated by separator
+ * @n: number of ints passed
+ * @separator: string to separate ints
+ * Return: always returns void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	if (!separator)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d", va_arg(args, int));
+		else
+			printf("%d%s", va_arg(args, int), separator);
+	}
+
+	printf("\n");
+
+}
