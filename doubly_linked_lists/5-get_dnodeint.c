cb003dd935d9afd35c3a70cf88ea46a567c8eabb
force-push-main <10535@holbertonstudents.com>
2025-04-01 04:10:33 +0000

    added code for task 5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Entry point
+ *
+ * Description: gets address of node at index
+ * @head: pointer to root node
+ * @index: index of node to find
+ * Return: address to found node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count;
+
+	if (!head)
+		return (NULL);
+
+	while (count < index && head)
+	{
+		head = head->next;
+		count++;
+	}
+
+	return (head);
+}
