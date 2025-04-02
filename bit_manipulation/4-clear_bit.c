4f77d1616f97286c0eec14fde5519c549e7c5c3c
force-push-main <10535@holbertonstudents.com>
2025-04-02 07:47:51 +0000

    added code for task 4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * clear_bit - Entry point
+ *
+ * Description: turns bit to 0 at index
+ * @n: num to transform
+ * @index: index to transform at
+ * Return: 1 or -1 if error
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int flipper;
+
+	if (index > 20)
+		return (-1);
+
+	if ((*n >> index | 0) == 0)
+		return (1);
+
+	flipper = 1;
+
+	flipper <<= index;
+
+	*n ^= flipper;
+
+	return (1);
+
+}
