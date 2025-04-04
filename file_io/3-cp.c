fe27d5ae826d6db3fb8f41985421bc83a7ccb617
force-push-main <10535@holbertonstudents.com>
2025-04-04 06:00:43 +0000

    fixed for files over 1024 chars

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,42 +1,39 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * _cp - Entry point
  *
  * Description: copies content of file to new file
- * @argc: number of args passed
- * @argv: args passed as array of strings
- * Return: 1 if success
+ * @from: file to copy from
+ * @to: file to copy to
+ * Return: void
  */
 
-int main(int argc, char **argv)
+void _cp(char *from, char *to)
 {
-	int fd_to, i, fd_from, r, w, c_to, c_from;
+	int fd_to, fd_from, w, r, c_to, c_from;
 	char buffer[1024];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fd_from = open(argv[1], O_RDONLY);
-	fd_to = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0666);
-	r = read(fd_from, &buffer, 1024);
-	for (i = 0; buffer[i];)
-		i++;
-	w = write(fd_to, &buffer, i);
+	fd_from = open(from, O_RDWR);
+	fd_to = open(to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	while ((r = read(fd_from, &buffer, 1024)) > 0)
+		w = write(fd_to, buffer, r);
+
 	if (fd_from < 0 || r < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
 		exit(98);
 	}
 	if (fd_to < 0 || w < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
 		exit(99);
 	}
+
 	c_to = close(fd_to);
 	c_from = close(fd_from);
+
 	if (c_from < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
@@ -48,5 +45,26 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-	return (1);
+}
+
+/**
+ * main - Entry point
+ *
+ * Description: copies content of file to new file
+ * @argc: number of args passed
+ * @argv: args passed as array of strings
+ * Return: 1 if success
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	_cp(argv[1], argv[2]);
+
+	return (0);
 }
