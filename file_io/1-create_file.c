a329cbff05783753f8937c5c97c105e75bad2fd3
force-push-main <10535@holbertonstudents.com>
2025-04-04 02:46:01 +0000

    added check for if string is null

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,13 +18,13 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
+	if (!text_content)
+		return (1);
+
 	i = 0;
 
 	while (text_content[i])
 		i++;
-
-	if (!i)
-		return (-1);
 
 	w = write(fd, text_content, i);
 
