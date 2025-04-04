d6db0b798bd79faeca16d107d76550dfe1ccfbb2
force-push-main <10535@holbertonstudents.com>
2025-04-04 01:23:45 +0000

    made code work

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,27 +12,30 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, write_return_value, read_return_value;
-	char *c;
+	char c;
+	size_t i;
 
 	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	
-	c = malloc(letters);
 
-	if (!c)
+	if (fd == -1)
 		return (0);
 
-	read_return_value = read(fd, c, letters);
+	for (i = 0; i < letters; i++)
+	{
+		read_return_value = read(fd, &c, 1);
+		if (read_return_value == 0)
+			return (0);
 
-	if (read_return_value == -1)
-		return(0);
-
-	write_return_value = write(1, c, letters);
+		write_return_value = write(STDOUT_FILENO, &c, 1);
+		if (write_return_value == -1)
+			return (0);
+	}
 
 	close(fd);
 
-	return (write_return_value);
+	return (i);
 
 }
